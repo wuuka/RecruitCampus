cc516f44099c4007e2e3e9965469a51563e0f50d
wuuka <769066052@qq.com>
2018-04-12 11:07:36 +0800

    两点之间路径个数

---
+++ 008 ShortestPath.cpp
@@ -0,0 +1,35 @@
+//两点之间路径个数
+//递归函数的案例  https://blog.csdn.net/sinat_38052999/article/details/73303111
+#include <iostream>  
+#include <cmath>  
+
+using namespace std;
+
+int fun(int row, int col, int x, int y) {
+	if (row == x&&col == y - 1)
+		return 1;
+	else if (row == x - 1 && col == y)
+		return 1;
+	else {
+		if (row<x&&col<y)
+			return fun(row + 1, col, x, y) + fun(row, col + 1, x, y);
+		else if (row<x&&col == y)
+			return fun(row + 1, col, x, y);
+		else if (row == x&&col<y)
+			return fun(row, col + 1, x, y);
+	}
+
+}
+
+int main()
+{
+	int startX, startY, endX, endY;
+	cout << "请输入起点和终点坐标" << endl;
+	cin >> startX >> startY >> endX >> endY;
+	cout << "起点坐标为（" << startX << "," << startY << ")" << endl;
+	cout << "终点坐标为（" << endX << "," << endY << ")" << endl;
+	int lenX = abs(startX - endX);
+	int lenY = abs(startY - endY);
+	cout << fun(0, 0, lenX, lenY);
+	return 0;
+}
