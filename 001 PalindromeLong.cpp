00c097e99ad51a9c5cd1f96ed22917bc8eb4e530
wuuka <769066052@qq.com>
2018-04-13 15:37:43 +0800

    Update 001 PalindromeLong.cpp

--- 001 PalindromeLong.cpp
+++ 001 PalindromeLong.cpp
@@ -1,6 +1,7 @@
-//[��Ѷ�����] �������
-//����һ���ַ���s������Դ���ɾ��һЩ�ַ���ʹ��ʣ�µĴ���һ�����Ĵ������ɾ������ʹ�û��Ĵ���أ�
-//�����Ҫɾ�����ַ�������
+//[腾讯编程题] 构造回文
+
+//给定一个字符串s，你可以从中删除一些字符，使得剩下的串是一个回文串。如何删除才能使得回文串最长呢？
+//输出需要删除的字符个数。
 
 //http://blog.csdn.net/sinat_35512245/article/details/53675660
 
@@ -12,7 +13,7 @@ using namespace std;
 const int MAXN = 1010;
 int temp[MAXN][MAXN];
 
-//����s�ķ���reverse��Ȼ�������ǵ���Ĺ��������У�Ҫɾ�����ַ���������֪��,ʱ�临�Ӷ�O(N^2)
+//先求s的反串reverse，然后求他们的最长的公共子序列，要删除的字符个数就能知道,时间复杂度O(N^2)
 int getRemoveNumber(string &s1)
 {
 	string s2(s1);
